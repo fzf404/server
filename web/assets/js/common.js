adcd00aefb07852e6d67559fa263b4aff15092f9
fzf404 <nmdfzf404@163.com>
2021-11-30 13:53:03 +0800

    ⚠ 控制台FBI-WARNING

--- web/assets/js/common.js
+++ web/assets/js/common.js
@@ -5,6 +5,29 @@
  * @Description: 通用代码
  */
 
+// 警告
+console.log(
+  `%c                                                  
+                                                  
+                                                  
+                 %c FBI WARNING %c                    
+                                                  
+                                                  
+%c     请遵守《中华人民共和国网络安全法》相关法律法规       
+                                                  
+            不要试图更改、破坏计算机系统               
+                                                  
+          违者移交司法机关, 追究其法律责任             
+                                                  
+                                                  
+                                                  
+`,
+  "background: #000; font-size: 18px; font-family: monospace",
+  "background: #f33; font-size: 18px; font-family: monospace; color: #eee; text-shadow:0 0 1px #fff",
+  "background: #000; font-size: 18px; font-family: monospace",
+  "background: #000; font-size: 18px; font-family: monospace; color: #ddd; text-shadow:0 0 2px #fff"
+);
+
 // const base_url = `http://${document.domain}:8080`;
 const base_url = `http://42.193.122.21:8080`;
 const admin_contact = "「QQ: 441535134」";
